72b7d2c38ec97d682642ab293bfd4ce0c9afe75c
Tom Souillard <tom.souillard@gmail.com>
2024-03-23 19:22:45 +0100

    [chore] Add constants file for global definitions

---
+++ dictionary_attack_tool/constants.py
@@ -0,0 +1,9 @@
+# Default settings for dictionary attack
+DEFAULT_MIN_LENGTH = 1
+DEFAULT_MAX_LENGTH = 8
+DEFAULT_CHARSET = 'aA1'
+
+# Paths and file names
+HASH_FILE = 'hash.txt'
+WORDLIST_FILE = 'wordlist.txt'
+RULES_FILE = 'rules.txt'
